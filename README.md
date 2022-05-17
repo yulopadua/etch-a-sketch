cfbdd712bf27a651c6f7433e5f68123870177883
yulopadua <yulo.padua@gmail.com>
2022-05-17 22:55:08 +0800

    Add event listener to set grid size and reset function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # etch-a-sketch
+To showcase my DOM manipulation skills, I've built a browser version of Etch-A-Sketch.
---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container');
+const setGridButton = document.getElementById('setGridButton');
+
+function reset() {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+setGridButton.addEventListener('click', () => {
+    let size = window.prompt("Set grid size", "16");
+
+    reset();
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        })
+    }
+})
+
